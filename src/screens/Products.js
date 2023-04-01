5ccddaf192964ecb423249399bd124a7f4c1f68d
omid beigi <o.beygi@gmail.com>
2023-04-01 11:33:39 +0330

    product screen added

---
+++ src/screens/Products.js
@@ -0,0 +1,36 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+} from "react-native";
+import products from "../data/products";
+
+const Products = () => {
+  return (
+    <FlatList
+      data={products}
+      renderItem={({ item }) => (
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: item.image }} style={styles.image} />
+        </View>
+      )}
+      numColumns={2}
+    />
+  );
+};
+
+export default Products;
+
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  itemContainer: {
+    width: "50%",
+    padding: 1,
+  },
+});
