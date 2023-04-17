fa76f85e19e998a6d2a297d2e33d9c738ee4c9be
omid beigi <o.beygi@gmail.com>
2023-04-17 08:35:42 +0330

    shopping cart added

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Products from "./src/screens/Products";
 import ProductDetails from "./src/screens/ProductDetails";
+import ShoppingCart from "./src/screens/ShoppingCart";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Products /> */}
-      <ProductDetails />
+      {/* <ProductDetails /> */}
+      <ShoppingCart/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/screens/ShoppingCart.js
@@ -0,0 +1,74 @@
+import { View, FlatList, StyleSheet, Text, Pressable } from "react-native";
+import cart from "../data/cart";
+import CartListItem from "../components/CartListItem";
+
+const ShoppingCartTotals = () => (
+  <View style={styles.totalsContainer}>
+    <View style={styles.row}>
+      <Text style={styles.text}>subtotal</Text>
+      <Text style={styles.text}>410,00 US$</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.text}>Delivery</Text>
+      <Text style={styles.text}>10,00 US$</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.textBold}>Total</Text>
+      <Text style={styles.textBold}>420,00 US$</Text>
+    </View>
+  </View>
+);
+
+const ShoppingCart = () => {
+  return (
+    <>
+      <FlatList
+        data={cart}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        ListFooterComponent={ShoppingCartTotals}
+      />
+      <Pressable style={styles.button}>
+        <Text style={styles.textButton}>Check out</Text>
+      </Pressable>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  totalsContainer: {
+    margin: 20,
+    borderColor: "gainsboro",
+    borderTopWidth: 1,
+    paddingTop: 10,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: "grey",
+  },
+  textBold: {
+    fontSize: 16,
+    fontWeight: "500",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
+  textButton: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
+
+export default ShoppingCart;
